46d3a0d0152e09c31dad023c4d585eba4795db56
wyang14 <william.1022@gmail.com>
2024-03-09 00:30:58 +0000

    fix: removed console log for sensitive data

--- server/api/models/index.get.ts
+++ server/api/models/index.get.ts
@@ -5,7 +5,6 @@ import { OPENAI_MODELS } from '@/server/utils/models';
 export default defineEventHandler(async (event) => {
   const { host, username, password } = event.context.ollama;
   const { x_openai_api_key: openai_api_key, x_anthropic_api_key } = event.context.keys;
-  console.log("openai_api_key: ", openai_api_key);
   const ollama = new Ollama({ host, fetch: FetchWithAuth.bind({ username, password }) });
   const response = await ollama.list();
 
