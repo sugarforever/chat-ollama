c9953aa76789703c583dc77e65a28df8f9fbd1a4
satrong <satrong@foxmail.com>
2024-03-12 11:06:18 +0800

    fix: remove the trailing slash in the URL

--- server/middleware/ollama.ts
+++ server/middleware/ollama.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler((event) => {
   const headers = getRequestHeaders(event);
-  const host = headers['x_ollama_host'] || 'http://localhost:11434';
+  const host = (headers['x_ollama_host'] || 'http://localhost:11434').replace(/\/$/, '');
   const username = headers['x_ollama_username'] || '';
   const password = headers['x_ollama_password'] || '';
 
