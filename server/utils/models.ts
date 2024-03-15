f91c9fd7230016783f61d645eade51bf70082673
wyang14 <william.1022@gmail.com>
2024-03-15 19:53:31 +0000

    feat: support Claude 3 Haiku

--- server/utils/models.ts
+++ server/utils/models.ts
@@ -22,6 +22,7 @@ export const OPENAI_EMBEDDING_MODELS = [
 ];
 
 export const ANTHROPIC_MODELS = [
+  "claude-3-haiku-20240307",
   "claude-3-opus-20240229",
   "claude-3-sonnet-20240229",
   "claude-2.1",
