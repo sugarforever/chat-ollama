34ee4fb5f45e47f7195a84162db6cf5193c0f6e7
wyang14 <william.1022@gmail.com>
2024-03-20 11:46:08 +0000

    RAG feat: parent document retriever with Redis docstore

--- server/api/models/chat/index.post.ts
+++ server/api/models/chat/index.post.ts
@@ -1,13 +1,13 @@
-import { Ollama } from 'ollama';
 import { Readable } from 'stream';
 import { AIMessage, HumanMessage } from "@langchain/core/messages";
 import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 import { ChatPromptTemplate, MessagesPlaceholder } from "@langchain/core/prompts";
 import { RunnablePassthrough, RunnableSequence } from "@langchain/core/runnables";
 import { setEventStreamResponse, FetchWithAuth } from '@/server/utils';
+import { ParentDocumentRetriever } from "langchain/retrievers/parent_document";
 import prisma from "@/server/utils/prisma";
 import { createChatModel, createEmbeddings } from '@/server/utils/models';
-import { createChromaVectorStore } from '@/server/utils/vectorstores';
+import { createRetriever } from '@/server/retriever';
 
 const SYSTEM_TEMPLATE = `Answer the user's questions based on the below context.
 Your answer should be in the format of Markdown.
@@ -20,7 +20,6 @@ If the context doesn't contain any relevant information to the question, don't m
 `;
 
 export default defineEventHandler(async (event) => {
-  const { host, username, password } = event.context.ollama;
   const { knowledgebaseId, model, messages, stream } = await readBody(event);
 
   if (knowledgebaseId) {
@@ -37,8 +36,8 @@ export default defineEventHandler(async (event) => {
     }
 
     const embeddings = createEmbeddings(knowledgebase.embedding, event);
-    const vectorStore = createChromaVectorStore(embeddings, `collection_${knowledgebase.id}`);
-    const retriever = vectorStore.asRetriever(4);
+    // const vectorStore = createChromaVectorStore(embeddings, `collection_${knowledgebase.id}`);
+    const retriever: ParentDocumentRetriever = await createRetriever(embeddings, `collection_${knowledgebase.id}`);
 
     const questionAnsweringPrompt = ChatPromptTemplate.fromMessages([
       ["system", SYSTEM_TEMPLATE],
@@ -50,7 +49,7 @@ export default defineEventHandler(async (event) => {
     const query = messages[messages.length - 1].content
     console.log("User query: ", query);
 
-    const relevant_docs = await retriever.invoke(query);
+    const relevant_docs = await retriever.getRelevantDocuments(query);
     console.log("Relevant documents: ", relevant_docs);
 
     const documentChain = await createStuffDocumentsChain({
