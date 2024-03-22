c32d10447863dfdd1d0afa45df6c812ca3e9cdcc
wyang14 <william.1022@gmail.com>
2024-03-22 23:51:53 +0000

    bugfix: google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,22 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/ui',
     '@vueuse/nuxt',
+    ['@nuxtjs/google-fonts', {
+      families: {
+        'Noto Sans': true,
+        'Josefin+Sans': true,
+        Lato: [100, 300],
+        Raleway: {
+          wght: [100, 400],
+          ital: [100]
+        },
+        Inter: '200..700',
+        'Crimson Pro': {
+          wght: '200..900',
+          ital: '200..700',
+        }
+      }
+    }]
   ],
   ui: {
     icons: ['heroicons', 'iconoir']
