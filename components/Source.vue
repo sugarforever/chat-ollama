85de86016f6d65d8e6affcf764da477a6d6ad19a
wyang14 <william.1022@gmail.com>
2024-04-13 00:38:04 +0100

    refactor: Sources and Source

---
+++ components/Source.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  source?: string
+}>()
+
+const isLink = computed(() => {
+  return props.source?.startsWith('http://') || props.source?.startsWith('https://')
+})
+</script>
+
+<template>
+  <div class="font-bold line-clamp-1 mb-2">
+    <a :href="props.source" v-if="isLink" class="underline" target="_blank" rel="noopener noreferrer">{{ props.source
+      }}</a>
+    <span v-else>{{ source }}</span>
+  </div>
+</template>
