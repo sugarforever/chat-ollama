c73ce0bdc82f9cc330a955ba1afa5904872a5a07
satrong <satrong@foxmail.com>
2024-04-12 11:16:35 +0800

    feat: order by id

--- server/api/instruction/index.get.ts
+++ server/api/instruction/index.get.ts
@@ -1,16 +1,20 @@
-import { type Instruction } from "@prisma/client";
-import prisma from "@/server/utils/prisma";
+import { type Instruction } from "@prisma/client"
+import prisma from "@/server/utils/prisma"
 
 const listInstructions = async (): Promise<Instruction[] | null> => {
   try {
-    return await prisma.instruction.findMany({});
+    return await prisma.instruction.findMany({
+      orderBy: {
+        id: 'desc'
+      }
+    })
   } catch (error) {
-    console.error("Error fetching instructions: ", error);
-    return null;
+    console.error("Error fetching instructions: ", error)
+    return null
   }
-};
+}
 
 export default defineEventHandler(async (event) => {
-  const instructions = await listInstructions();
-  return { instructions };
-});
+  const instructions = await listInstructions()
+  return { instructions }
+})
