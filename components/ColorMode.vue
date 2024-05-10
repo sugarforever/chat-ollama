063253a1fb26aaf7f1cb03d2c17297235cc6c572
satrong <satrong@foxmail.com>
2024-05-10 11:30:40 +0800

    feat: color mode supports following system settings

--- components/ColorMode.vue
+++ components/ColorMode.vue
@@ -1,23 +1,50 @@
 <script setup lang="ts">
+const { t } = useI18n()
 const colorMode = useColorMode()
-const isDark = computed({
-  get() {
-    return colorMode.value === 'dark'
-  },
-  set() {
-    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
-  }
+
+const icons = ['i-material-symbols-desktop-windows-outline-rounded', 'i-heroicons-sun-20-solid', 'i-heroicons-moon-20-solid']
+
+const list = computed(() => {
+  return [
+    { label: t('colorMode.system'), value: 'system', icon: icons[0] },
+    { label: t('colorMode.light'), value: 'light', icon: icons[1] },
+    { label: t('colorMode.dark'), value: 'dark', icon: icons[2] },
+  ]
+})
+
+const currentIcon = ref(list.value[0].icon)
+const items = computed(() => {
+  return list.value.map(item => {
+    return {
+      ...item,
+      click: () => {
+        colorMode.preference = item.value
+        currentIcon.value = item.icon
+      }
+    }
+  })
+})
+
+onMounted(() => {
+  currentIcon.value = list.value.find(el => el.value === colorMode.preference)?.icon || ''
 })
 </script>
+
 <template>
-  <ClientOnly>
-    <UButton :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+  <UDropdown :items="[items]"
+             :ui="{ width: 'w-32' }"
+             :popper="{ placement: 'bottom-start' }">
+    <UButton :icon="currentIcon"
              color="gray"
              variant="ghost"
              aria-label="Theme"
-             @click="isDark = !isDark" />
-    <template #fallback>
-      <div class="w-8 h-8" />
+             @touchstart.stop />
+
+    <template #item="{ item }">
+      <div class="flex items-center" :class="{ 'text-primary-500': item.value === colorMode.preference }">
+        <UIcon :name="item.icon" class="opacity-70 text-lg" />
+        <span class="ml-2">{{ item.label }}</span>
+      </div>
     </template>
-  </ClientOnly>
+  </UDropdown>
 </template>
