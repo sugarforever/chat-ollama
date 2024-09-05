e5c4a35b62d688bdbf9d51b9d81ae7868597546e
satrong <satrong@foxmail.com>
2024-09-05 10:42:17 +0800

    feat: support custom the default page

--- composables/useMenus.ts
+++ composables/useMenus.ts
@@ -1,8 +1,11 @@
+
+export const DEFAULT_PAGE_LINK = '/welcome'
+
 export function useMenus() {
   const { t } = useI18n()
 
   return computed(() => [
-    { label: t('menu.home'), icon: 'i-heroicons-home', to: '/' },
+    { label: t('menu.home'), icon: 'i-heroicons-home', to: '/welcome' },
     { label: t('menu.models'), icon: 'i-heroicons-rectangle-stack', to: '/models' },
     { label: t('menu.instructions'), icon: 'i-iconoir-terminal', to: '/instructions' },
     { label: t('menu.knowledgeBases'), icon: 'i-heroicons-book-open', to: '/knowledgebases' },
