a266e74af619d545815dcd906118bab052a5db3d
sugarforever <william.1022@gmail.com>
2025-09-08 23:23:18 +0100

    added untracked files

---
+++ components/ArtifactButton.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+defineEmits<{
+  click: []
+}>()
+</script>
+
+<template>
+  <UTooltip text="Preview" :popper="{ placement: 'left' }">
+    <UButton 
+      icon="i-heroicons-eye-20-solid"
+      color="primary"
+      variant="ghost"
+      size="xs"
+      class="mt-1 opacity-0 transition-all duration-300 ease-out hover:scale-105 hover:shadow-[0_4px_12px_rgba(59,130,246,0.15)] dark:hover:shadow-[0_4px_12px_rgba(59,130,246,0.25)]"
+      @click="$emit('click')" 
+    />
+  </UTooltip>
+</template>
---
+++ components/MessageImages.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+interface Props {
+  images: string[]
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div v-if="images.length > 0" class="my-3 grid gap-2 grid-cols-[repeat(auto-fit,minmax(200px,1fr))]">
+    <img 
+      v-for="(url, index) in images"
+      :key="index"
+      :src="url"
+      class="w-full h-auto rounded-lg max-h-64 object-contain bg-gray-100 dark:bg-gray-800 hover:cursor-pointer hover:opacity-90 transition-opacity"
+      :alt="`Image ${index + 1}`" 
+    />
+  </div>
+</template>
